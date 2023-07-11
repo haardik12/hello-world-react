f49fe531e97acc1139bf2d7764c39d386fff65f3
haardik12 <80949318+haardik12@users.noreply.github.com>
2023-07-12 04:47:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>my major skills</h1>
+      <h2>
+        in this project ill breifly explain some of my major skills and
+        strengths.
+      </h2>
+      <ul>
+        <li>i'm willing to learn new skills that'll benefit my future</li>
+        <li>
+          as a freelancer for my whole career i find solutions on my own and
+          always explore new ways to do a task
+        </li>
+        <li>i always look for new ways to do a task efficiently</li>
+      </ul>
+    </div>
+  );
+}
